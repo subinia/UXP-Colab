f9de78c53eced89e0abc8145d1f1c2f6dcc612e8
Uk Lee <uklee@tangunsoft.com>
2021-10-15 11:59:00 +0900

    Update uxpin.config.js

--- uxpin.config.js
+++ uxpin.config.js
@@ -5,7 +5,7 @@ module.exports = {
         name: "Layout",
         include: [
           "src/DeviceViewer/DeviceViewer.js",
-          "src/Iframe/Iframe.js",
+//          "src/Iframe/Iframe.js",
           "src/Grid/Grid.js",
           "src/GridList/GridList.js",
           "src/GridListTile/GridListTile.js",
